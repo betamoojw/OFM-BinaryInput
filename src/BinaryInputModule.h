1f0d30c6a1338266164c35cc463b9eb22d329594
Ing-Dom <dom@ing-dom.de>
2024-04-02 09:37:39 +0200

    derived from SOM-UP, restricted to a simple usage with plain GPIOs used as input and made it flexible in terms of channel count. WIP, builds without errors but untested.

---
+++ src/BinaryInputModule.h
@@ -0,0 +1,25 @@
+#include "BinaryInputChannel.h"
+#include "OpenKNX/Module.h"
+#include "hardware.h"
+
+class BinaryInputModule : public OpenKNX::Module
+{
+
+  public:
+    const std::string name() override;
+    const std::string version() override;
+    void loop() override;
+    void setup() override;
+    void setPins(uint8_t* gpioPins);
+
+  private:
+    void processHardwareInputs();
+
+    uint32_t _lastHardwareQuery = 0;
+
+    uint8_t* _gpioPins = nullptr;
+
+    BinaryInputChannel* _channels[BI_ChannelCount];
+};
+
+extern BinaryInputModule openknxBinaryInputModule;
